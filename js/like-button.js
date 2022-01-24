48c2029e441333e8c71fe6c2a1415a886ca1e495
Luna Hogart <moontochka@gmail.com>
2022-01-24 20:32:34 +0700

    autotest

--- js/like-button.js
+++ js/like-button.js
@@ -1,3 +1,23 @@
-let like = document.querySelector('.element__like-button');
-like.addEventListener( 'click', () =>
-like.classList.toggle('element__like-button_active') );
+document.querySelector('#like-1').addEventListener('click', () => {
+  document.querySelector('#like-1').classList.toggle('element__like-button_active');
+});
+
+document.querySelector('#like-2').addEventListener('click', () => {
+  document.querySelector('#like-2').classList.toggle('element__like-button_active');
+});
+
+document.querySelector('#like-3').addEventListener('click', () => {
+  document.querySelector('#like-3').classList.toggle('element__like-button_active');
+});
+
+document.querySelector('#like-4').addEventListener('click', () => {
+  document.querySelector('#like-4').classList.toggle('element__like-button_active');
+});
+
+document.querySelector('#like-5').addEventListener('click', () => {
+  document.querySelector('#like-5').classList.toggle('element__like-button_active');
+});
+
+document.querySelector('#like-6').addEventListener('click', () => {
+  document.querySelector('#like-6').classList.toggle('element__like-button_active');
+});
