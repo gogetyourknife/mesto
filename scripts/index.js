ea15490c732e8101e8b35e060e1230c725a1c89a
Luna Hogart <moontochka@gmail.com>
2022-02-22 09:00:58 +0700

    валидация форм

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 // инпуты для форм
 
-const nameInput = document.querySelector('.popup__input_type_name');
-const descrInput = document.querySelector('.popup__input_type_description');
+const nameInput = document.querySelector('.popup__input_type-name');
+const descrInput = document.querySelector('.popup__input_type-description');
 const newName = document.querySelector('.profile__name');
 const newDescr = document.querySelector('.profile__description');
 const descrImagePopup = document.querySelector('.popup__description');
@@ -31,9 +31,13 @@ const openImagePopup = document.querySelector('.popup_type_zoom');
 
 // переменные для ПР5
 
-/* let card = document.querySelectorAll('.element__card'); */
 const template = document.querySelector('#template').content;
 const cardsContainer = document.querySelector('.element');
+
+// пр6
+
+const mainScreen = document.querySelector('.root');
+
 
 // переменные для создания карточки
 const initialCards = [
@@ -67,6 +71,7 @@ const initialCards = [
 
 function openPopup(item) {
   item.classList.add('popup_opened');
+  document.addEventListener('keydown', keyHandlerEscape);
 };
 
 function openPropfilePopup (evt) {
@@ -76,7 +81,6 @@ function openPropfilePopup (evt) {
   openPopup(popupEdit);
 };
 
-
 function openPopupImage (place, link) {
   popupImageLink.src = link;
   popupImageDescription.textContent = place;
@@ -85,6 +89,7 @@ function openPopupImage (place, link) {
 
 function closePopup(item) {
   item.classList.remove('popup_opened');
+  document.removeEventListener('keydown', keyHandlerEscape);
 }
 
 // Сохранение изменений в профиле
@@ -96,6 +101,22 @@ function saveNewName (evt) {
   closePopup(popupEdit);
 }
 
+// ловим эскейп
+
+function keyHandlerEscape (evt) {
+  if (evt.key === 'Escape') {
+    closePopup(document.querySelector('.popup_opened'));
+  }
+};
+
+// ловим клик мышки
+
+function mouseHandler (evt) {
+  if (evt.target === evt.currentTarget) {
+    closePopup(evt.target);
+  }
+};
+
 // Создание новой карточки
 
 function createCard(place, link) {
@@ -104,18 +125,11 @@ function createCard(place, link) {
   newCard.querySelector('.element__image').src = link;
   newCard.querySelector('.element__title').textContent = place;
 
-  newCard.querySelector('.element__delete-button').addEventListener('click', function (event) {
-    event.target.closest('.element__card').remove();
-  })
-
-  newCard.querySelector('.element__like-button').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('element__like-button_active');
-  });
-
   newCard.querySelector('.element__image').addEventListener('click', function (evt) {
     evt.preventDefault;
     openPopupImage(place, link)
   });
+
   return newCard;
 };
 
@@ -147,12 +161,39 @@ popupAdd.addEventListener('click', () => {openPopup(popupAddCard)});
 popupClosed.forEach((closed) => {
   closed.addEventListener('click', function() {
     popup.forEach((item) => {
-      closePopup(item)
+      closePopup(item);
     });
   });
 });
 
 formElement.addEventListener('submit', saveNewName);
 
+// делегирование слушателей
 
+cardsContainer.addEventListener('click', function (evt) {
+  if (evt.target.classList.contains('element__delete-button')) {
+     evt.target.closest('.element__card').remove();
+ }
+});
 
+cardsContainer.addEventListener('click', function (evt) {
+  if (evt.target.classList.contains('element__like-button')) {
+    evt.target.classList.toggle('element__like-button_active');
+ }
+});
+
+// добавление при нажатии на энтер
+
+[nameInput, descrInput, place, link].forEach(item => {
+  item.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      saveNewName();
+    }
+  });
+});
+
+// закрытие при нажатии мышкой
+
+popupEdit.addEventListener('click', mouseHandler);
+popupAddCard.addEventListener('click', mouseHandler);
+openImagePopup.addEventListener('click', mouseHandler);
---
+++ scripts/validate.js
@@ -0,0 +1,79 @@
+// показываем ошибку валидации
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input-error');
+  errorElement.classList.add('form__input-error_active');
+  errorElement.textContent = errorMessage;
+};
+
+// убираем ошибку валидации
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input-error');
+  errorElement.classList.remove('form__input-error_active');
+  errorElement.textContent = '';
+};
+
+// проверяем валидацию
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+};
+
+// перекрашиваем кнопку
+
+const setSubmitButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__save-button_disabled');
+    buttonElement.setAttribute('disabled', '');
+  } else {
+    buttonElement.classList.remove('popup__save-button_disabled');
+    buttonElement.removeAttribute('disabled', '');
+  }
+};
+
+// функция-обработчик
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__save-button');
+  setSubmitButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      setSubmitButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+// передаем обработчик в основную функцию
+
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+   formList.forEach((formElement) => {
+   formElement.addEventListener('submit', (evt) => {
+     evt.preventDefault();
+   });
+     setEventListeners(formElement);
+ });
+ }
+
+ // элементы деструктуризации
+
+  enableValidation({
+    formElement: '.popup__form',
+    inputElement: '.popup__input',
+    errorMessage: 'form__input-error'
+  });
